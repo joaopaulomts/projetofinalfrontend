d3f93b9d6260e91d2c6bc524d435212c56c68eb1
Joao Augusto Da Silva Borges <joao.935620@alunosatc.edu.br>
2024-06-03 21:16:22 -0300

    Task_6

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
+import Tabela from './routes/tabela/Tabela.jsx'
+import Formulario from './routes/formulario/Formulario.jsx'
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Tabela />
+      },
+      {
+        path: 'cadastrar',
+        element: <Formulario />
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
---
+++ src/routes/formulario/Formulario.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+import { useOutletContext } from "react-router-dom"
+import "./Formulario.css"
+
+export default function Formulario(){
+  const { form, handleChange, saveProduct } = useOutletContext()
+
+  return (
+    <div className="container">
+      <h2>Cadastro de Produtos</h2>
+      <form onSubmit={saveProduct}>
+        <label htmlFor="name">Nome:</label>
+        <input type="text" name="name" value={form.name} onChange={handleChange} required />
+        <label htmlFor="price">Preço:</label>
+        <input type="number" name="price" value={form.price} min={0} onChange={handleChange} required />
+        <label htmlFor="stock">Estoque:</label>
+        <input type="number" name="stock" value={form.stock} min={0} onChange={handleChange} required />
+        <input type="submit" value="Cadastrar" />
+      </form>
+    </div>
+  )
+}
